886380d2e9b11eeea33479d51139634d2c9c36c3
“why” <why@163.com>
2019-05-12 17:14:15 +0800

    创建了Cat类

---
+++ GithubDemo/GithubDemo/Cat.h
@@ -0,0 +1,17 @@
+//
+//  Cat.h
+//  GithubDemo
+//
+//  Created by wind on 2019/5/12.
+//  Copyright © 2019 JLK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cat : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GithubDemo/GithubDemo/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  GithubDemo
+//
+//  Created by wind on 2019/5/12.
+//  Copyright © 2019 JLK. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
