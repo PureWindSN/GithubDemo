d7e51301dcc1416e92687896689d9bae9e1d33ee
Wind <1983457078@qq.com>
2019-06-05 16:00:43 +0800

    1.测试单一terminal方式实现提交至githbu\n2.基于1，实现commit message换行测试

--- GithubDemo/GithubDemo/ViewController.m
+++ GithubDemo/GithubDemo/ViewController.m
@@ -25,7 +25,10 @@
     
     NSLog(@"测试tag添加失败原因");
     
-    //测试单一terminal方式提交至github
+    //测试单一terminal方式提交至github 4.0
+    
+    
+    //测试单一terminal方式提交至github & commit message换行 5.0
     
 }
 
